0e733b5d6c2ae07eb0be264fc14fad4a56d4c293
asikarwar <asikarwar007@gmail.com>
2017-09-14 22:53:39 +0530

    v0.08

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createLocationMessage',(coords)=>{
-    io.emit('newLocationMessage',generateLocationMessage('admin',coords.latitude}, coords.longitude});
+    io.emit('newLocationMessage',generateLocationMessage('admin',coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
--- server/utils/message.js
+++ server/utils/message.js
@@ -10,8 +10,8 @@ var generateLocationMessage = (from,latitude,longitude) =>{
 	return {
 		from,
 		url :`https://www.google.com/maps?q=${latitude},${longitude}`,
-		createdAt:new Date(),getTime()
-	}
-}
+		createdAt:new Date().getTime()
+	};
+};
 
 module.exports = {generateMessage,generateLocationMessage};
