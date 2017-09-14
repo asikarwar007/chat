5ae98190808a8349b33cdf095039e1a691f9bade
asikarwar <asikarwar007@gmail.com>
2017-09-14 21:50:41 +0530

    v0.04

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,28 @@
+var socket = io();
 
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
 
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
 
-	var socket = io();
-	socket.on('connect', function () {
-		console.log('connected');
-		
-		socket.emit('CreateMessage',{
-			to:'abhsd@gmail.com',
-			text:'asjdsdjh'
-		});
-	});
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
 
-	socket.on('disconnect',function () {
-		console.log('disconnect from server');
-	});
+  jQuery('#messages').append(li);
+});
 
-	socket.on('newMessage',function (message) {
-		console.log('new message',message);
-	});
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
+});
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http =require('http');
 
+const {generateMessage} = require('./utils/message');
+
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -12,20 +14,21 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
-		console.log('new user');
+		console.log('new user connected');
 
-		socket.emit('newMessage',{
-			from:'abhi@example.com',
-			text:'text text',
-			createAt:123
-		});
+ 		socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+ 		socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-		socket.on('createMessage',(newMessage)=>{
-			console.log('createMessage',newMessage);
-		})
+
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
+    
+  });
 
 	socket.on('disconnect',()=>{
-	console.log('user disconnect');
+	console.log('user disconnected');
 })
 		});
 
